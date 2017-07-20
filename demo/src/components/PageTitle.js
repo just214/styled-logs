5b150fd3090773f756aa7d71b5c5b0544735122e
gojutin <gojutin@gmail.com>
2017-07-20 09:28:43 -0400

    Updated meta descriptions

--- demo/src/components/PageTitle.js
+++ demo/src/components/PageTitle.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
 const Title = styled.h1`
   color: #333;
@@ -11,5 +12,8 @@ const PageTitle = _=>
     { _.title}
   </Title>
 
+PageTitle.propTypes = {
+  title: PropTypes.string,
+}
 
 export default PageTitle;
