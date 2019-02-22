716654e354897f4054680701fc6cc860c49e6f28
Justin Wilkerson <JW@justins-mbp.lan>
2019-02-22 08:09:29 -0500

    Updated version

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,27 +1,27 @@
 declare type MaybeStringArray = string[] | Function[] | any[] | undefined[];
 declare function styled(previousStyle?: any): ((strings: TemplateStringsArray, ...values: MaybeStringArray) => {
-    (strings: TemplateStringsArray, ...values: MaybeStringArray): void;
+    (strings: string | TemplateStringsArray, ...values: MaybeStringArray): void;
     style: string;
     methodName: string;
 }) | undefined;
 declare namespace styled {
     var log: (strings: TemplateStringsArray, ...values: MaybeStringArray) => {
-        (strings: TemplateStringsArray, ...values: MaybeStringArray): void;
+        (strings: string | TemplateStringsArray, ...values: MaybeStringArray): void;
         style: string;
         methodName: string;
     };
     var info: (strings: TemplateStringsArray, ...values: MaybeStringArray) => {
-        (strings: TemplateStringsArray, ...values: MaybeStringArray): void;
+        (strings: string | TemplateStringsArray, ...values: MaybeStringArray): void;
         style: string;
         methodName: string;
     };
     var warn: (strings: TemplateStringsArray, ...values: MaybeStringArray) => {
-        (strings: TemplateStringsArray, ...values: MaybeStringArray): void;
+        (strings: string | TemplateStringsArray, ...values: MaybeStringArray): void;
         style: string;
         methodName: string;
     };
     var error: (strings: TemplateStringsArray, ...values: MaybeStringArray) => {
-        (strings: TemplateStringsArray, ...values: MaybeStringArray): void;
+        (strings: string | TemplateStringsArray, ...values: MaybeStringArray): void;
         style: string;
         methodName: string;
     };
